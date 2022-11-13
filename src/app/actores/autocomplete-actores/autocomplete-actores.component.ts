ab04499a666e3c0d92a58337399aebc53063d3c8
Nico <nicoyoles@gmail.com>
2022-11-13 16:30:58 -0300

    merge 67 - 1630 13112022

--- src/app/actores/autocomplete-actores/autocomplete-actores.component.ts
+++ src/app/actores/autocomplete-actores/autocomplete-actores.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { MatTable } from '@angular/material/table';
 
 @Component({
   selector: 'app-autocomplete-actores',
@@ -13,21 +15,28 @@ export class AutocompleteActoresComponent implements OnInit {
   actores = [
     {
       nombre: 'nico',
+      personaje: '',
       foto: 'https://es.web.img2.acsta.net/pictures/14/04/30/11/55/592219.jpg',
     },
     {
       nombre: 'carlos',
-      foto: 'https://es.web.img2.acsta.net/pictures/14/04/30/11/55/592219.jpg',
+      personaje: '',
+      foto: 'https://upload.wikimedia.org/wikipedia/en/2/2a/Enchantedposter.jpg',
     },
     {
       nombre: 'albert',
-      foto: 'https://es.web.img2.acsta.net/pictures/14/04/30/11/55/592219.jpg',
+      personaje: '',
+      foto: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/mcdpran-ec143-1655225829.jpg?crop=1.00xw:0.952xh;0,0.0485xh&resize=1200:*',
     },
   ];
 
   actoresOriginal = this.actores;
 
   actoresSeleccionados = [];
+
+  columnasAMostrar = ['imagen', 'nombre', 'personaje', 'acciones'];
+
+  @ViewChild(MatTable) table: MatTable<any>;
 
   ngOnInit(): void {
     this.control.valueChanges.subscribe((valor) => {
@@ -42,5 +51,24 @@ export class AutocompleteActoresComponent implements OnInit {
     console.log(event.option.value);
     this.actoresSeleccionados.push(event.option.value);
     this.control.patchValue('');
+    if (this.table !== undefined) {
+      this.table.renderRows();
+    }
+  }
+
+  eliminar(actor) {
+    const indice = this.actoresSeleccionados.findIndex(
+      (a) => a.nombre === actor.nombre
+    );
+    this.actoresSeleccionados.splice(indice, 1);
+    this.table.renderRows();
+  }
+
+  finalizaArrastre(event: CdkDragDrop<any[]>){
+    const indicePrevio = this.actoresSeleccionados.findIndex(
+      actor => actor === event.item.data
+    )
+    moveItemInArray(this.actoresSeleccionados, indicePrevio, event.currentIndex);
+    this.table.renderRows();
   }
 }
