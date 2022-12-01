35728115ef94c7150a09d9beffc1a1c143369184
Nico <nicoyoles@gmail.com>
2022-12-01 19:09:51 -0300

    115 1909 01122022

--- src/app/peliculas/detalle-pelicula/detalle-pelicula.component.ts
+++ src/app/peliculas/detalle-pelicula/detalle-pelicula.component.ts
@@ -8,46 +8,51 @@ import { PeliculasService } from '../peliculas.service';
 @Component({
   selector: 'app-detalle-pelicula',
   templateUrl: './detalle-pelicula.component.html',
-  styleUrls: ['./detalle-pelicula.component.css']
+  styleUrls: ['./detalle-pelicula.component.css'],
 })
 export class DetallePeliculaComponent implements OnInit {
-
-  constructor(private peliculasService: PeliculasService,
+  constructor(
+    private peliculasService: PeliculasService,
     private activatedRoute: ActivatedRoute,
-    private sanitizer: DomSanitizer) { }
+    private sanitizer: DomSanitizer
+  ) {}
 
   pelicula: PeliculaDTO;
   fechaLanzamiento: Date;
   trailerURL: SafeResourceUrl;
-  coordenadas: Coordenada[];
-
+  coordenadas: Coordenada[] = [];
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe(params =>
-      this.peliculasService.obtenerPorId(params.id).subscribe(pelicula =>{
+    this.activatedRoute.params.subscribe((params) =>
+      this.peliculasService.obtenerPorId(params.id).subscribe((pelicula) => {
         console.log(pelicula);
         this.pelicula = pelicula;
         this.fechaLanzamiento = new Date(this.pelicula.fechaLanzamiento);
         this.trailerURL = this.generarURLYoutubeEmbed(this.pelicula.trailer);
-        this.coordenadas = pelicula.cines.map(cine => {
-          return {longitud:cine.longitud, latitud: cine.latitud, mensaje: cine.nombre}
-        })
-      }))
+        this.coordenadas = pelicula.cines.map((cine) => {
+          return {
+            longitud: cine.longitud,
+            latitud: cine.latitud,
+            mensaje: cine.nombre,
+          };
+        });
+      })
+    );
   }
 
-  generarURLYoutubeEmbed(url: any):SafeResourceUrl{
-    if (!url){
+  generarURLYoutubeEmbed(url: any): SafeResourceUrl {
+    if (!url) {
       return '';
     }
 
     var video_id = url.split('v=')[1];
     var posicionAmpersand = video_id.indexOf('&');
-    if (posicionAmpersand !== -1){
+    if (posicionAmpersand !== -1) {
       video_id = video_id.substring(0, posicionAmpersand);
     }
 
-    return this.sanitizer
-    .bypassSecurityTrustResourceUrl(`https://www.youtube.com/embed/${video_id}`);
+    return this.sanitizer.bypassSecurityTrustResourceUrl(
+      `https://www.youtube.com/embed/${video_id}`
+    );
   }
-
 }
